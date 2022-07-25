1be73d52bee798d2039108d57119141170c20e67
[maracana] <[postmaster@journalmaracana.com]>
2022-07-25 09:34:33 +0100

    fix tags

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -20,7 +20,7 @@ console.log("tag", tag)
       <>
       {/* <a href={tag.href}       className={`nc-Tag inline-block   text-sm text-neutral-600 py-2 px-3 rounded-lg border border-neutral-100 md:py-2.5 md:px-4 dark:bg-neutral-700 dark:border-neutral-700 hover:border-neutral-200 dark:hover:border-neutral-6000 ${className}`} */}
       <a href={"/tags?"+`${tag.tag}`}  className={`nc-Tag inline-block text-sm text-neutral-600 dark:text-neutral-300 py-2 px-3 rounded-lg  border-neutral-100 md:py-2.5 md:px-4 bg-neutral-50 dark:bg-neutral-700 dark:border-neutral-700 hover:border-neutral-200 dark:hover:border-neutral-6000  ${className}`}>
-     {type == 'sidebar' ? `${tag.tag}` : `${tag.tag}`} 
+     {type == 'sidebar' ? `${tag.tag}` : `${tag}`} 
       {!hideCount && (
          <span className="text-xs font-normal"> ({tag.count})</span>
       )}
